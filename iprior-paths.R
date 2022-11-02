c5c279f44fd7812a37d00a4fe069ad7eb66d397b
Haziq Jamil <haziq.jamil@ubd.edu.bn>
2022-11-02 09:12:25 +0800

    Update iprior-paths.R

--- iprior-paths.R
+++ iprior-paths.R
@@ -1,12 +1,13 @@
 library(iprior)
 library(tidyverse)
+navyblue <- "#002f5c"
 
-n <- 8
+n <- 150
 dat <- tibble(
   x = seq(0, 2 * pi, length = n),
   y = sin(x) #+ rnorm(n, sd = 0.01)
 )
-mod <- iprior(y ~ x, dat, kernel = "se", control = list(silent = TRUE))
+# mod <- iprior(y ~ x, dat, kernel = "se", control = list(silent = TRUE))
 psi <- 1 #get_psi(mod)
 lambda <-  1  # get_lambda(mod)
 N <- 100
@@ -15,11 +16,10 @@ B <- 100
 # I-prior sample paths ---------------------------------------------------------
 prior.samp <- tibble(x = seq(0, 2 * pi, length = N))
 H <- kern_se(prior.samp$x)
-
 for (i in 1:B) {
   w <- rnorm(N, mean = 0, sd = sqrt(psi))
   prior.samp <- cbind(prior.samp, mean(dat$y) + 
-                        as.numeric(lambda * H %*% w / 2))
+                        as.numeric(lambda * H %*% w))
 }
 colnames(prior.samp) <- c("x", paste0("y", 1:B))
 prior.samp <- reshape2::melt(prior.samp, id = "x")
@@ -37,7 +37,7 @@ Vy <- psi * (H * lambda) %*% (H * lambda) + diag(1/psi, n)
 w_hat <- as.numeric(psi * lambda * H %*% solve(Vy) %*% (dat$y - mean(dat$y)))
 w <- mvtnorm::rmvnorm(B, mean = w_hat, sigma = solve(Vy))
 h <- kern_se(dat$x, post.samp$x)
-for (i in 1:100) {
+for (i in 1:B) {
   post.samp <- cbind(post.samp, mean(dat$y) + as.numeric(lambda * h %*% w[i, ]))
 }
 colnames(post.samp) <- c("x", paste0("y", 1:B))
@@ -55,7 +55,19 @@ bind_rows(
 ) %>%
   ggplot(aes(x, value, group = variable)) +
   geom_line(size = 0.2, alpha = 0.4, col = "gray50") +
-  geom_point(data = dat, aes(x, y), inherit.aes = FALSE) +
+
+  geom_line(data = as_tibble(prior.samp) %>% 
+              group_by(x) %>% 
+              summarise(value = mean(value)) %>%
+              mutate(type = "prior"),
+            aes(x, value), col = "red3", size = 1.5, inherit.aes = FALSE) +
+  geom_line(data = as_tibble(post.samp) %>% 
+              group_by(x) %>% 
+              summarise(value = mean(value)) %>%
+              mutate(type = "posterior"),
+            aes(x, value), col = "red3", size = 1.5, inherit.aes = FALSE) +
+  geom_point(data = dat, aes(x, y), inherit.aes = FALSE,
+             shape = 1) +  
   facet_grid(. ~ type) +
   theme_classic() +
   coord_cartesian(ylim = c(-2, 2))
