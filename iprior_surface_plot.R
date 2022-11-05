0e15d544e0aca33cc913225598980f1f8800ca5f
Haziq Jamil <haziq.jamil@ubd.edu.bn>
2022-11-05 22:11:00 +0800

    Update estimation

--- iprior_surface_plot.R
+++ iprior_surface_plot.R
@@ -5,10 +5,10 @@ mod <- iprior(y ~ X, gen_smooth(), kernel = "fbm")
 no.points <- 50
 x <- log(get_lambda(mod))
 x <- seq(x - 20, x + 15, length = no.points)  # lambda
-# x <- sort(c(x, log(get_lambda(mod))))
+x <- sort(c(x, log(get_lambda(mod))))
 y <- log(get_psi(mod))
 y <- seq(y - 10, y + 15, length = no.points)  # psi
-# y <- sort(c(y, log(get_psi(mod))))
+y <- sort(c(y, log(get_psi(mod))))
 tab <- expand.grid(x = x, y = y)
 
 # Calculate the log-likelihood for each point in the grid
@@ -16,8 +16,8 @@ z <- rep(NA, nrow(tab))
 for (i in seq_along(z)) {
   z[i] <- logLik(mod, theta = as.numeric(tab[i, ]))
 }
-tab.loglik <- matrix(z, nrow = no.points , ncol = no.points )
-zmin <- -5000
+tab.loglik <- matrix(z, nrow = no.points + 1, ncol = no.points + 1)
+zmin <- -4000
 
 # 3D plot
 plot_ly(x = x, y = y, z = tab.loglik, zauto = FALSE, zmin = zmin) %>%
@@ -30,14 +30,12 @@ plot_ly(x = x, y = y, z = tab.loglik, zauto = FALSE, zmin = zmin) %>%
       zaxis = list(title = "Log-likelihood",  range = c(zmin, max(z)))
     ))
 
-
-as_tibble(tab) %>%
-  mutate(z = z) %>%
-  filter(z > zmin) %>%
-  ggplot(aes(x, y, fill = z, col = z)) +
-  # geom_point(aes(col = z)) +
-  geom_raster() +
-  geom_contour(aes(z = z), col = "white") +
-  scale_fill_viridis_c() +
-  scale_colour_viridis_c() +
-  theme_classic()
+# as_tibble(tab) %>%
+#   mutate(z = z) %>%
+#   filter(z > zmin) %>%
+#   ggplot(aes(x, y, fill = z, col = z)) +
+#   geom_raster() +
+#   geom_contour(aes(z = z), col = "white") +
+#   scale_fill_viridis_c() +
+#   scale_colour_viridis_c() +
+#   theme_classic()
